3e706c69d5e6f870fe759c152c083db241fdda33
Yaroslav Nikonorov <yar@MacBook-Pro-Yaroslav.local>
2017-09-26 18:18:54 +0300

    Added docstrings and removed executor

--- cloudshell-L1-{{cookiecutter.project_slug}}/main.py
+++ cloudshell-L1-{{cookiecutter.project_slug}}/main.py
@@ -5,10 +5,13 @@ import sys
 from datetime import datetime
 
 from cloudshell.core.logger.qs_logger import get_qs_logger
+from cloudshell.layer_one.core.command_executor import CommandExecutor
 from cloudshell.layer_one.core.driver_listener import DriverListener
 from cloudshell.layer_one.core.helper.runtime_configuration import RuntimeConfiguration
 from cloudshell.layer_one.core.helper.xml_logger import XMLLogger
-from {{cookiecutter.project_slug}}.driver_command_executor import DriverCommandExecutor
+from {{cookiecutter.project_slug}}.driver_commands import DriverCommands
+
+SERVER_PORT = 1024
 
 if __name__ == '__main__':
     driver_name = '{{cookiecutter.project_slug.upper()}}'
@@ -30,17 +33,19 @@ if __name__ == '__main__':
     command_logger = get_qs_logger(log_group=driver_name,
                                    log_file_prefix=driver_name + '_commands', log_category='COMMANDS')
     log_level = runtime_config.read_key('LOGGING.LEVEL')
-    print log_level
     if log_level:
         command_logger.setLevel(log_level)
 
     command_logger.debug('Starting driver {}'.format(driver_name))
 
+    # Creating driver commands instance
+    driver_instance = DriverCommands(command_logger)
+
     # Creating command executor instance
-    command_executor = DriverCommandExecutor(command_logger)
+    command_executor = CommandExecutor(driver_instance, command_logger)
 
     # Creating listener instance
     server = DriverListener(command_executor, xml_logger, command_logger)
 
-    # Start listening socket
-    server.start_listening(port=sys.argv[1] if len(sys.argv) > 1 else None)
+    # Start listening
+    server.start_listening(port=sys.argv[1] if len(sys.argv) > 1 else SERVER_PORT)
--- cloudshell-L1-{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/driver_commands.py
+++ cloudshell-L1-{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/driver_commands.py
@@ -11,39 +11,176 @@ class DriverCommands(DriverCommandsInterface):
 
     def __init__(self, logger):
         """
-        :param logger: 
+        :param logger:
+        :type logger: logging.Logger
         """
         self._logger = logger
 
+    def login(self, address, username, password):
+        """
+        Perform login operation on the device
+        :param address: resource address, "192.168.42.240"
+        :param username: username to login on the device
+        :param password: password
+        :return: None
+        :raises Exception: if command failed
+        Example:
+            session = CliSession(address, username, password)
+            session.connect()
+        """
+        raise NotImplementedError
+
     def get_state_id(self):
-        pass
+        """
+        Check if CS synchronized with the device.
+        :return: Synchronization ID, GetStateIdResponseInfo(-1) if not used
+        :rtype: cloudshell.layer_one.core.response.response_info.GetStateIdResponseInfo
+        :raises Exception: if command failed
+
+        Example:
+            state_id = self._state_flow.get_id()
+            return GetStateIdResponseInfo(state_id)
+        """
+        raise NotImplementedError
 
     def set_state_id(self, state_id):
-        pass
+        """
+        Set synchronization state id to the device, called after Autoload or SyncFomDevice commands
+        :param state_id: synchronization ID
+        :type state_id: str
+        :return: None
+        :raises Exception: if command failed
+
+        Example:
+            self._state_flow.set_id(state_id)
+        """
+        raise NotImplementedError
 
     def map_bidi(self, src_port, dst_port):
-        pass
+        """
+        Create a bidirectional connection between source and destination ports
+        :param src_port: src port address, '192.168.42.240/1/21'
+        :type src_port: str
+        :param dst_port: dst port address, '192.168.42.240/1/22'
+        :type dst_port: str
+        :return: None
+        :raises Exception: if command failed
+
+        Example:
+            self._map_flow.map_bidi(src_port, dst_port)
+        """
+        raise NotImplementedError
 
     def map_uni(self, src_port, dst_port):
-        pass
+        """
+        Unidirectional mapping of two ports
+        :param src_port: src port address, '192.168.42.240/1/21'
+        :type src_port: str
+        :param dst_port: dst port address, '192.168.42.240/1/22'
+        :type dst_port: str
+        :return: None
+        :raises Exception: if command failed
+
+        Example:
+            self._map_flow.map_uni(src_port, dst_port)
+        """
+        raise NotImplementedError
 
     def get_resource_description(self, address):
-        pass
+        """
+        Auto-load function to retrieve all information from the device
+        :param address: resource address, '192.168.42.240'
+        :type address: str
+        :return: resource description
+        :rtype: cloudshell.layer_one.core.response.response_info.ResourceDescriptionResponseInfo
+        :raises cloudshell.layer_one.core.layer_one_driver_exception.LayerOneDriverException: Layer one exception.
+
+        Example:
+
+            from cloudshell.layer_one.core.response.resource_info.entities.chassis import Chassis
+            from cloudshell.layer_one.core.response.resource_info.entities.blade import Blade
+            from cloudshell.layer_one.core.response.resource_info.entities.port import Port
+
+            chassis_resource_id = chassis_info.get_id()
+            chassis_address = chassis_info.get_address()
+            chassis_model_name = "{{ cookiecutter.model_name }} Chassis"
+            chassis_serial_number = chassis_info.get_serial_number()
+            chassis = Chassis(resource_id, address, model_name, serial_number)
+
+            blade_resource_id = blade_info.get_id()
+            blade_model_name = 'Generic L1 Module'
+            blade_serial_number = blade_info.get_serial_number()
+            blade.set_parent_resource(chassis)
+
+            port_id = port_info.get_id()
+            port_serial_number = port_info.get_serial_number()
+            port = Port(port_id, 'Generic L1 Port', port_serial_number)
+            port.set_parent_resource(blade)
+
+            return ResourceDescriptionResponseInfo([chassis])
+        """
+        raise NotImplementedError
 
     def map_clear(self, ports):
-        pass
+        """
+        Remove simplex/multi-cast/duplex connection ending on the destination port
+        :param ports: ports, ['192.168.42.240/1/21', '192.168.42.240/1/22']
+        :type ports: list
+        :return: None
+        :raises Exception: if command failed
 
-    def login(self, address, username, password):
-        pass
+        Example:
+            for port in ports:
+                self._map_flow.map_clear(port)
+        """
+        raise NotImplementedError
 
     def map_clear_to(self, src_port, dst_port):
-        pass
+        """
+        Remove simplex/multi-cast/duplex connection ending on the destination port
+        :param src_port: src port address, '192.168.42.240/1/21'
+        :type src_port: str
+        :param dst_port: dst port address, '192.168.42.240/1/22'
+        :type dst_port: str
+        :return: None
+        :raises Exception: if command failed
+
+        Example:
+            self._map_flow.map_clear_to(src_port, dst_port)
+        """
+        raise NotImplementedError
 
     def get_attribute_value(self, cs_address, attribute_name):
-        pass
+        """
+        Retrieve attribute value from the device
+        :param cs_address: address, '192.168.42.240/1/21'
+        :type cs_address: str
+        :param attribute_name: attribute name, "Port Speed"
+        :type attribute_name: str
+        :return: attribute value
+        :rtype: cloudshell.layer_one.core.response.response_info.AttributeValueResponseInfo
+        :raises Exception: if command failed
+
+        Example:
+            value = self._attribute_flow.get(cs_address, attribute_name)
+            return AttributeValueResponseInfo(value)
+        """
+        raise NotImplementedError
 
     def set_attribute_value(self, cs_address, attribute_name, attribute_value):
-        pass
+        """
+        Set attribute value to the device
+        :param cs_address: address, '192.168.42.240/1/21'
+        :type cs_address: str
+        :param attribute_name: attribute name, "Port Speed"
+        :type attribute_name: str
+        :param attribute_value: value, "10000"
+        :type attribute_value: str
+        :return: attribute value
+        :rtype: cloudshell.layer_one.core.response.response_info.AttributeValueResponseInfo
+        :raises Exception: if command failed
+        """
+        raise NotImplementedError
 
     def map_tap(self, src_port, dst_port):
         return self.map_uni(src_port, dst_port)
