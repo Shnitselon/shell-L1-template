2f00475730e88ad1ca2ac8923b2b9153a514615a
kalsky <yanivka@gmail.com>
2017-10-24 12:20:23 -0700

    fix names in the compile_driver batch

--- cloudshell-L1-{{cookiecutter.project_slug}}/compile_driver.bat
+++ cloudshell-L1-{{cookiecutter.project_slug}}/compile_driver.bat
@@ -1,3 +1,3 @@
 pyinstaller --onefile driver.spec
-copy datemodel\*.xml dist /Y
-copy {{cookiecutter.project_slug}}_runtime_configuration.json dist /Y
+copy datamodel\*.xml dist /Y
+copy {{cookiecutter.project_slug}}_runtimeconfig.json dist /Y
