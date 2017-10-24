06d12fa7ca9fbb21f7676ffd9f240e44eead352d
kalsky <yanivka@gmail.com>
2017-10-24 12:26:46 -0700

    runtimeconfig is yml not json

--- cloudshell-L1-{{cookiecutter.project_slug}}/compile_driver.bat
+++ cloudshell-L1-{{cookiecutter.project_slug}}/compile_driver.bat
@@ -1,3 +1,3 @@
 pyinstaller --onefile driver.spec
 copy datamodel\*.xml dist /Y
-copy {{cookiecutter.project_slug}}_runtimeconfig.json dist /Y
+copy {{cookiecutter.project_slug}}_runtimeconfig.yml dist /Y
