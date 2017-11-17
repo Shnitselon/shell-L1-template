29d17ed24a520d812ecc74825fb1d66564567a66
Yaroslav Nikonorov <yaroslav.n@quali.com>
2017-11-17 15:50:42 +0200

    Fixed paths in the driver start script

--- cloudshell-L1-{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.bat
+++ cloudshell-L1-{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.bat
@@ -3,8 +3,8 @@ setlocal
 set SERVER_FOLDER=C:\Program Files (x86)\QualiSystems\CloudShell\Server
 set DRIVERS_FOLDER=%SERVER_FOLDER%\Drivers
 set DRIVER_NAME=%~n0
-set LOGS_PATH=%SERVER_FOLDER%\Logs\%DRIVER_NAME%
-set DRIVER_ENV=%DRIVERS_FOLDER%\cloudshell-L1-%DRIVER_NAME%
+set LOGS_PATH="%SERVER_FOLDER%\Logs"
+set DRIVER_ENV=%DRIVERS_FOLDER%\%DRIVER_NAME%
 set PYTHON="%DRIVER_ENV%\Scripts\python"
 set EXE=%PYTHON% "%DRIVER_ENV%\main.py"
 set port=%1
