15a0ec2c6494147b2e692765e3d37912dbc359f1
Yaroslav Nikonorov <yaroslav.n@quali.com>
2017-12-06 17:19:01 +0200

    Added DEBUG and fixed main logger

--- cloudshell-L1-{{cookiecutter.project_slug}}/main.py
+++ cloudshell-L1-{{cookiecutter.project_slug}}/main.py
@@ -32,10 +32,8 @@ if __name__ == '__main__':
     # Creating command logger instance
     command_logger = get_qs_logger(log_group=driver_name,
                                    log_file_prefix=driver_name + '_commands', log_category='COMMANDS')
-    log_level = runtime_config.read_key('LOGGING.LEVEL')
-    if log_level:
-        command_logger.setLevel(log_level)
-
+    log_level = runtime_config.read_key('LOGGING.LEVEL', 'INFO')
+    command_logger.setLevel(log_level)
     command_logger.debug('Starting driver {}'.format(driver_name))
 
     # Creating driver commands instance
