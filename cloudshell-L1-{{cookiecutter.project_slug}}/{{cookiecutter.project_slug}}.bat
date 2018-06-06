5c3142d220edf1d88e83aaf02250c3ebe67b71ae
Yaroslav Nikonorov <yaroslav.n@quali.com>
2018-06-06 15:14:53 +0300

    Fixed batch file

--- cloudshell-L1-{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.bat
+++ cloudshell-L1-{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 setlocal
-set SERVER_FOLDER=C:\Program Files (x86)\QualiSystems\CloudShell\Server
-set DRIVERS_FOLDER=%SERVER_FOLDER%\Drivers
+set DRIVERS_FOLDER=%~dp0
+set SERVER_FOLDER=%DRIVERS_FOLDER%\..
 set DRIVER_NAME=%~n0
 set LOGS_PATH="%SERVER_FOLDER%\Logs"
 set DRIVER_ENV=%DRIVERS_FOLDER%\cloudshell-L1-%DRIVER_NAME%
